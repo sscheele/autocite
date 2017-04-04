d15821a16448d59f4cfa6d503505618904a30bf5
Sam <sam.scheele@outlook.com>
2017-04-03 20:33:09 -0400

    clarify schema, finish and test preprocessing

--- main.js
+++ main.js
@@ -2,10 +2,37 @@ var parse5 = require('parse5');
 var request = require('request');
 var fs = require('fs');
 
+//TODO: more weighs for NN layer 2 (weights -> thresholds -> weights -> threshold -> answer)
 var config = JSON.parse(fs.readFileSync('config.json').toString());
+var badTags = ['head', 'link', 'script', 'nav', 'form'];
 
 function writeConfig() {
     fs.writeFile('config.json', JSON.stringify(config));
+}
+
+//trim irrelevant tags (the ones in badTags) and add content variables
+function preprocess(doc) {
+    if (!doc.childNodes) {
+        return;
+    }
+    var totalWords = 0;
+    for (var i = 0; i < doc.childNodes.length; i++) {
+        if (doc.childNodes[i].nodeName == '#text') {
+            totalWords += doc.childNodes[i].value.split(" ").length;
+        }
+        if (doc.childNodes[i].tagName && badTags.indexOf(doc.childNodes[i].tagName) != -1) {
+            doc.childNodes.splice(i, 1); //remove if a useless tag, then continue
+            i--; //decrement i because we're removing from the list over which we're iterating
+            continue;
+        }
+        preprocess(doc.childNodes[i]); //yeeeeeeah, recursion in a for loop
+    }
+    doc.isContent = totalWords > config.minContentWords;
+}
+
+function propagateContentUp(doc){
+    //TODO: if more than detcontentpercent of an element is content, mark that element as content as well
+    //use DFS-style recursion so that content tags propagate up
 }
 
 (function () {
@@ -18,18 +45,20 @@ function writeConfig() {
     var urlRegex = /^(https?:\/\/)?([a-zA-Z0-9]+\.)?[a-zA-Z0-9]+\.[a-zA-Z0-9]{1,7}(\/.+)*\/?$/;
 
     for (var i = 0; i < lines.length; i++) {
-        if (!urlRegex.test(lines[i])){
+        if (!urlRegex.test(lines[i])) {
             console.log(lines[i], ' is not a valid URL');
             continue;
         }
         var url = lines[i];
         request(lines[i], function (error, response, body) {
             console.log(url + ':');
-            if (error){
+            if (error) {
                 console.log('error:', error); // Print the error if one occurred
                 return;
             }
-            console.log('body:', body); // Print the HTML
+            var doc = parse5.parse(body); // Parse the HTML
+            preprocess(doc);
+            console.log(parse5.serialize(doc));
         });
     }
 })();
